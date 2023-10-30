dadff894961ecf0119ec183d4a7c429da1888ebe
hideto1198 <hideto1198@yahoo.co.jp>
2023-10-31 00:38:06 +0900

    Baseの使用例を追加

--- Sources/CustomMacroLibClient/main.swift
+++ Sources/CustomMacroLibClient/main.swift
@@ -7,3 +7,15 @@ struct Test {
     var propertyWithSameName: String
 }
 
+struct State: Equatable {
+    var base: Base
+}
+
+@Base(propertyName: "base")
+extension State {
+    struct Base: Equatable {
+        var property1: String
+        var property2: Bool
+        var property3: Int
+    }
+}
