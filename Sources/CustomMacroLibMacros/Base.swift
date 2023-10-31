c1cbd774647bec52cc2911f21b870d47dba1029d
hideto1198 <hideto1198@yahoo.co.jp>
2023-10-31 10:41:49 +0900

    アンチパターン時にエラーを表示するように変更

--- Sources/CustomMacroLibMacros/Base.swift
+++ Sources/CustomMacroLibMacros/Base.swift
@@ -14,11 +14,18 @@ enum CustomError: Error {
 
 public struct Base: MemberMacro {
     public static func expansion(of node: AttributeSyntax, providingMembersOf declaration: some DeclGroupSyntax, in context: some MacroExpansionContext) throws -> [DeclSyntax] {
+        // extensionにのみ有効
+        guard declaration.as(ExtensionDeclSyntax.self) != nil else { throw CustomError.message("このマクロはExtensionにのみ有効です") }
+        // extension *.Stateにのみ有効
+        guard let extensionName = declaration.as(ExtensionDeclSyntax.self)?.extendedType.description,
+              extensionName.contains(".State")
+        else { throw CustomError.message("Stateの拡張でのみ使用可能です") }
+        // 引数の値が空の時エラー
         guard let parentPropertyName = node.arguments?.as(LabeledExprListSyntax.self)?.first?.expression.as(StringLiteralExprSyntax.self)?.segments.first?.as(StringSegmentSyntax.self)?.content.text,
               !parentPropertyName.isEmpty
-        else { throw CustomError.message("Invaild arguments")}
+        else { throw CustomError.message("引数が無効です")}
         let memberList = declaration.memberBlock.members
-        
+
         let cases = memberList.compactMap({ member -> String? in
             guard member.decl.as(StructDeclSyntax.self)?.name.text == "Base" else { return nil }
             let members = member.decl.as(StructDeclSyntax.self)?.memberBlock.members.compactMap({ variableMember -> String? in
